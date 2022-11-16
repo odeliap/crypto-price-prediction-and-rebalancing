29034f1d0aba8eeeac9259e55893fcec32cbef36
Odelia Putterman <odeliaputterman@gmail.com>
2022-11-15 16:33:15 -0800

    rework pipeline flow

--- src/app.py
+++ src/app.py
@@ -48,36 +48,32 @@ start_date = end_date - timedelta(days=days_to_subtract)
 end_date_str = end_date.strftime(date_format)
 start_date_str = start_date.strftime(date_format)
 
-kick_off_pipeline = st.button('Kick off pipeline')
+retrieve_predictions = st.button('Retrieve predicted future prices')
 
-if kick_off_pipeline:
-    with st.spinner(f'Retrieving previous daily prices'):
-        for coin in cryptocurrencies.keys():
-            prev_stock_prices = yf.download(cryptocurrencies.get(coin), start=start_date_str, end=end_date_str,
-                                            interval='1d')
-            prev_stock_prices_clean = prev_stock_prices.dropna()
-            st.subheader(f'{coin.capitalize()} previous {n_steps_in} daily prices')
-            st.write(prev_stock_prices_clean)
-        time.sleep(10)
-    with st.spinner(f'Scraping news'):
-        for coin in cryptocurrencies.keys():
+collective_predicted_prices = pd.DataFrame()
+
+if retrieve_predictions:
+    # TODO: run this in parallel instead
+    for coin in cryptocurrencies.keys():
+        with st.spinner(f'Retrieving {coin} previous daily prices'):
+            prev_prices = yf.download(cryptocurrencies.get(coin), start=start_date_str, end=end_date_str, interval='1d')
+            prev_prices_clean = prev_prices.dropna()
+            time.sleep(10)
+        with st.spinner(f'Scraping {coin} news'):
             dataframe = newsScraper(coin)
-            st.subheader(f'{coin.capitalize()} scraped news for past {n_steps_in} days')
-            st.write(dataframe)
-        time.sleep(10)
-    with st.spinner(f'Getting sentiment for found news'):
-        for coin in cryptocurrencies.keys():
+            time.sleep(10)
+        with st.spinner(f'Getting sentiment for found {coin} news'):
             # TODO: format previous stock prices with sentiment dataframe to make input to sentiment analyzer
             #sentiment_dataframe = SentimentAnalyzer(dataframe).dataframe
             time.sleep(10)
-    with st.spinner(f'Retrieving predicted prices'):
-        for coin in cryptocurrencies.keys():
+        with st.spinner(f'Retrieving predicted prices for {coin}'):
             #predictions = predict(sentiment_dataframe, coin, n_steps_in)
+            # TODO: add predicted prices to collective_predicted_prices dataframe
             time.sleep(10)
 
-    #rebalanced_portfolio, stock_returns = rebalancingAlgorithm(predicted_prices, numStocks, numRev)
+#rebalance_portfolio, stock_returns = rebalancingAlgorithm(predicted_prices, numStocks, numRev)
 
-    #logging.info("Rebalanced Portfolio Performance")
-    #logging.info("CAGR: " + str(CAGR(rebalanced_portfolio, start_date, end_date)))
-    #logging.info("Sharpe Ratio: " + str(sharpe_ratio(rebalanced_portfolio, 0.03, start_date, end_date)))
-    #logging.info("Maximum Drawdown: " + str(maximum_drawdown(rebalanced_portfolio)))
+#logging.info("Rebalanced Portfolio Performance")
+#logging.info("CAGR: " + str(CAGR(rebalanced_portfolio, start_date, end_date)))
+#logging.info("Sharpe Ratio: " + str(sharpe_ratio(rebalanced_portfolio, 0.03, start_date, end_date)))
+#logging.info("Maximum Drawdown: " + str(maximum_drawdown(rebalanced_portfolio)))
